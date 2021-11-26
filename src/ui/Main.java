31e655a9cb1166763d311a4e834cf90aa776a72e
Juan Camilo <juancaram21@gmail.com>
2021-11-25 20:06:46 -0500

    Second commit

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,13 @@
 package ui;
 
+/**
+ * This is Main Class.
+ * @version 1
+ * @author Juan Felipe Castillo Gomez, https://github.com/JuanFCast
+ * @author Juan Camilo Ramirez Tabares, https://github.com/JCamiloRamirezTabares
+ * @author Jesus David Rodriguez Burbano, https://github.com/JesusD03
+ */
+
 public class Main {
 
 }
