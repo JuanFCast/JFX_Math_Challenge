3c3197c0094e499f67ff2d39d9b0be02bcc3aa2c
Juan Felipe Castillo Gomez <juanfelipe.castillogomez@gmail.com>
2021-11-28 18:02:13 -0500

    add skeleton Top Pane

--- src/ui/MathChallengeGUI.java
+++ src/ui/MathChallengeGUI.java
@@ -1,5 +1,20 @@
 package ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class MathChallengeGUI {
+
+
+
+	//TOP PANE
+	@FXML
+	private TableView<?> tableTop;
+
+	@FXML
+	private TableColumn<?, ?> colNickname;
+
+	//TOP PANE
 
 }
