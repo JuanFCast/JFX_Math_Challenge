deae5ab5f877d89d48f777806d378e023b86cef2
Jesus David Rodriguez Burbano <jesusdavidrodriguezburbano@gmail.com>
2021-11-30 17:36:04 -0500

    add method addChallenger in ScoreBoard.class

--- src/model/ScoreBoard.java
+++ src/model/ScoreBoard.java
@@ -5,7 +5,33 @@ public class ScoreBoard {
 	
 	private Player root;
 	
+	public ScoreBoard() {
+		
+	}
 	
-	
+	public void addChallenger(Player n) {
+		if(root == null) {
+			root = n;
+		}else {
+			addChallenger(n, root);
+		}
+	}
+	//Para este caso manejaremos puntajes menores o iguales asignados a la izquierda, y puntajes mayores
+	//para la derecha
+	public void addChallenger(Player n, Player r) {
+		if(n.getScore() <= r.getScore()) {
+			if(r.getLeft() != null) {
+				addChallenger(n, r.getLeft());
+			} else {
+				r.setLeft(n);
+			}
+		} else {
+			if(r.getRight() != null) {
+				addChallenger(n, r.getRight());
+			} else {
+				r.setRight(n);
+			}
+		}
+	}
 
 }
