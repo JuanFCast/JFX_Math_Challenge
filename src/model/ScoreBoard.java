38cb6f39bda58e7e0c7238882e8999984c67b817
Jesus David Rodriguez Burbano <jesusdavidrodriguezburbano@gmail.com>
2021-11-30 18:40:32 -0500

    add method search and changes

--- src/model/ScoreBoard.java
+++ src/model/ScoreBoard.java
@@ -18,19 +18,41 @@ public class ScoreBoard {
 	}
 	//Para este caso manejaremos puntajes menores o iguales asignados a la izquierda, y puntajes mayores
 	//para la derecha
-	public void addChallenger(Player n, Player r) {
+	private void addChallenger(Player n, Player r) {
 		if(n.getScore() <= r.getScore()) {
 			if(r.getLeft() != null) {
 				addChallenger(n, r.getLeft());
 			} else {
 				r.setLeft(n);
+				n.setUp(r);
 			}
 		} else {
 			if(r.getRight() != null) {
 				addChallenger(n, r.getRight());
 			} else {
 				r.setRight(n);
+				n.setUp(r);
 			}
+		}
+	}
+	
+	public Player search(long score) {
+		if(root == null) {
+			return null;
+		}else {
+			return search(root, score);
+		}
+	}
+	
+	private Player search(Player current, long score) {
+		if(current == null) {
+			return current;
+		}else if(current.getScore() == score) {
+			return current;
+		}else if(score > current.getScore()) {
+			return search(current.getRight(), score);
+		}else {
+			return search(current.getLeft(), score);
 		}
 	}
 
