3242f70948759f9abf76f1521c1691d63b6e1a28
Jesus David Rodriguez Burbano <jesusdavidrodriguezburbano@gmail.com>
2021-12-02 11:27:31 -0500

    add method removePlayer again xD

--- src/model/ScoreBoard.java
+++ src/model/ScoreBoard.java
@@ -1,9 +1,9 @@
 package model;
 
 /*
- * Añadir (Para añadir cada jugador al arbol binario) ya
+ * Añadir (Para añadir cada jugador al arbol binario)			 ya
  * Eliminar (Para borrar determinado jugador)
- * Buscar (Para buscar un jugador especifico) ya 
+ * Buscar (Para buscar un jugador especifico)					 ya 
  * Retornar un arreglo con los primeros 5 jugadores
  * Retornar la posicion del jugador que esta jugando
  * Leer cada jugador del docs que los guardara
@@ -65,6 +65,77 @@ public class ScoreBoard {
 			return search(current.getRight(), score);
 		}else {
 			return search(current.getLeft(), score);
+		}
+	}
+	
+	public void remove(long score) {
+		Player pRem = search(score);
+		removePlayer(pRem);
+	}
+	
+	private void removePlayer(Player rem) {
+		if(rem != null) {
+			if(rem.getLeft() == null && rem.getRight() == null) { // CASO 1 el nodo es una hoja
+				if(rem == root) {			// el arbol solo tiene un elemento
+					root = null;
+				}else if(rem == rem.getUp().getLeft()) {
+					rem.getUp().setLeft(null);
+				}else {
+					rem.getUp().setRight(null);
+				}
+			}else if(rem.getLeft() == null || rem.getRight() == null) {	// CASO 2 el nodo tiene un solo hijo
+				Player child;
+				if(rem.getLeft()!=null) {	//tiene un hijo izquierdo
+					child = rem.getLeft();
+				}else {						//tiene un hijo derecho
+					child = rem.getRight();
+				}
+				child.setUp(rem.getUp());
+				if(rem == root) {
+					root = child;
+				}else if(rem == rem.getUp().getLeft()) {		//el nodo a eliminar es un hijo izq
+					rem.getUp().setLeft(child);
+				}else {											//el nodo a eliminar es un hijo der
+					rem.getUp().setRight(child);
+				}
+			}else {							// CASO 3 el nodo tiene dos hijos
+				Player succesor = min(rem.getRight());
+				rem.setName(succesor.getName());			//le pasamos los datos del succesor al nodo
+				rem.setScore(succesor.getScore());			//que queremos eliminar
+				removePlayer(succesor);						//ahora borramos el duplicado
+			}
+		}
+	}
+	
+	public Player minimun() {
+		if(root == null) {
+			return root;
+		} else {
+			return min(root);
+		}
+	}
+	
+	private Player min(Player current) {
+		if(current.getLeft()==null) {
+			return current;
+		}else {
+			return min(current.getLeft());
+		}
+	}
+	
+	public Player maximun() {
+		if(root == null) {
+			return root;
+		} else {
+			return max(root);
+		}
+	}
+	
+	private Player max(Player current) {
+		if(current.getRight()==null) {
+			return current;
+		}else {
+			return max(current.getRight());
 		}
 	}
 
